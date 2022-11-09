0bb2423274da5bccf2cda333b3d41a022aecb70e
saad321123 <117503765+saad321123@users.noreply.github.com>
2022-11-09 15:38:26 +0530

    Add file via upload

---
+++ 4_chap_lists.py
@@ -0,0 +1,21 @@
+# create a list using []
+a=[20, 23, 25,26]
+# print the list using print function
+print(a)
+# Access using index using a[0],a[1],a[2]
+print(a [2])
+#change the value of list using
+a[0]=22
+print(a)
+
+#We can create a list with item of different type
+c=[26,"Saad",9.7,False]
+print(c)
+
+
+# List slicing
+office=["Amin sir","Rameez sir","Sai kishor","Aman","Saad",22]
+print(office[0:4])
+print(office[-4:])
+
+
