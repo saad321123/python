ab9ddcde34310e0b438c25a5020bb7d66d8a4fda
saad321123 <117503765+saad321123@users.noreply.github.com>
2022-11-11 16:29:28 +0530

    Add files via upload

---
+++ 4_chap_tuple.py
@@ -0,0 +1,21 @@
+# Creatind a tuple using ()
+#s=(1,3,5,7,9)
+
+#s1=() #empty tuple
+
+#s1=(1) #wrong way to declare a Tuple with single element
+
+#s1=(1,) # Tuple with single element 
+#print(s1)
+# Printing the element of tuple 
+# print(s[0])
+
+# we cannot the update value of a tuple
+# s[0]=45
+
+
+  #Tuple Methods
+
+s=(1,2,3,4,5,1,3,1)
+print(s.count(1)) #count
+print(s.index(4)) #index
